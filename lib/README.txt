fc42d4a701abda66c44588eba2f7bfa1b0f03579
Schen <schen.git@gmail.com>
2022-12-27 18:11:15 +0900

    u08 binutils and crosscompiler setup

---
+++ lib/README.txt
@@ -0,0 +1,36 @@
+The source code of:
+
+- binutils-2.39 (2022-08-05)
+- gcc-12.2.0 (2022-12-27)
+
+```sh
+wget -O- https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz | tar xvJ
+wget -O- https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz | tar xvJ
+
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+export MYSRC=$PWD
+
+# make and install Binutils
+cd $MYSRC
+mkdir build-binutils
+cd build-binutils
+../binutils-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+# make and install GCC cross compiler
+cd $MYSRC
+# The $PREFIX/bin dir _must_ be in the PATH. We did that above.
+which -- $TARGET-as || echo $TARGET-as is not in the PATH
+ 
+mkdir build-gcc
+cd build-gcc
+../gcc-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
+```
+
---
+++ u08-crosscompile_gcc_compiler/src/boot/boot.asm
@@ -0,0 +1,86 @@
+[BITS 16]
+[ORG 0x7c00]
+
+CODE_SEG equ GDT_CODE - GDT_START
+DATA_SEG equ GDT_DATA - GDT_START
+
+_START:
+    jmp short START
+    nop
+
+times 33 db 0						; Fake BPB (BIOS Parameter Block), 36 bytes (BPB) - 3 bytes (JMP SHORT START NOP) = 33 bytes
+
+START:
+    jmp 0:STEP2
+
+STEP2:
+    cli
+    mov ax, 0x00
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+.LOAD_PROTECTED:
+    cli
+    lgdt[GDT_DESCRIPTOR]				; load GDT with GDTR
+    mov eax, cr0
+    or al, 1						; set PE (Protection Enable) bit in CR0 (Control Register 0)
+    mov cr0, eax
+
+    ; to load CS with proper PM32 descriptor
+    jmp CODE_SEG:LOAD32					 ; Perform far jump to selector 08h (offset into GDT, pointing at a 32bit PM code segment descriptor)
+
+; GDT
+GDT_START:
+GDT_NULL:						; GDT Entry 0
+    dd 0						; 4 bytes
+    dd 0
+
+; OFFSET 0x0
+GDT_CODE:						; CS should point to this
+    dw 0xffff						; Segment Limit, first 0-15 bits, 0xffff is 256MB in 4kb page
+    dw 0						; Base Address 15:00
+    db 0						; Base Address 23:16
+    db 10011010b					; Common Access Byte 0x9a for Kernel Mode Code Segment
+    db 11001111b					; Simply allow all memory access
+    db 0						; Base 31:24
+
+GDT_DATA:						; DS, SS, ES, FS, GS
+    dw 0xffff						; Segment Limit, first 0-15 bits, 0xffff is 256MB in 4kb page
+    dw 0						; Base Address 15:00
+    db 0						; Base Address 23:16
+    db 0x92						; Access Byte for Kernel Mode Data Segment
+    db 11001111b
+    db 0						; Base 31:24
+
+GDT_END:
+
+GDT_DESCRIPTOR:						; GDTR
+    dw GDT_END - GDT_START - 1				; GDT_Size - 1, because max GDT_Size can be 65536, 1 byte more than 0xffff
+    dd GDT_START					; Offset
+
+[BITS 32]
+LOAD32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+
+enable_a20_line:
+    in al, 0x92
+    test al, 2
+    jnz after
+    or al, 2
+    and al, 0xFE
+    out 0x92, al
+after:
+    jmp $
+
+times 510 - ($ - $$) db 0	; pad to write the boot signature 0x55 0xAA
+dw 0xAA55			; The boot signature in Little Endian
