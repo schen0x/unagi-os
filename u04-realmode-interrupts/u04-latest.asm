89433129554ff42010d355b5d3a7e5179a71da2b
Schen <schen.git@gmail.com>
2023-04-05 11:59:01 +0900

    u04 Interrupt Vector Table revisit

--- u04-realmode-interrupts/u04-latest.asm
+++ u04-realmode-interrupts/u04-latest.asm
@@ -10,11 +10,11 @@ times 33 db 0						; Fake BPB (BIOS Parameter Block), 36 bytes (BPB) - 3 bytes (
 START:
     jmp 0x7c0:STEP2
 
-HANDLE_ZERO:
+HANDLE_ZERO:						; The interrupt 0 subroutine
     mov ah, 0eh
     mov al, 'A'
     mov bx, 0x00
-    int 0x10
+    int 0x10						; Print the char 'A' onto
     iret
 
 STEP2:
@@ -25,13 +25,14 @@ STEP2:
     mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
-    sti
+    sti								; Enable Interrupts
 
-    mov word[ss:0x00], HANDLE_ZERO			; 2 bytes; The offset, if not ss not specified, will be default to use ds
-    mov word[ss:0x02], 0x7c0				; 2 bytes; if not ss not specified, the default to use ds
-    int 0						; call our int0 manually
+	; directly modify the IVT table entry 0, which is the 4 bytes start from 0x0
+    mov word[ss:0x00], HANDLE_ZERO	; The IVT Offset; mov 2 bytes; `ss` is necessary because otherwise `ds` is used by default during a `mov` instruction
+    mov word[ss:0x02], 0x7c0		; The IVT Segment
+    int 0							; Invoke the Interrupt 0
     mov ax, 0x00
-    div ax						; int0 also happens to be divide by 0 exception, we can trigger it by div 0
+    div ax							; int0 also happens to be divide by 0 exception, we can trigger it by div 0
     mov si, MESSAGE
     call PRINT
     jmp $
